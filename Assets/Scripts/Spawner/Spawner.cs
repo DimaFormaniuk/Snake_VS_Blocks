96b3db80cdd8f1bbaff59197b5081015634983c9
DimaFormaniuk <44363635+DimaFormaniuk@users.noreply.github.com>
2021-01-22 14:55:20 +0200

    home task

--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -15,15 +15,22 @@ public class Spawner : MonoBehaviour
     [Header("Wall")]
     [SerializeField] private Wall _wallTemplate;
     [SerializeField] private int _wallSpawnChance;
+    [Header("Bonus")]
+    [SerializeField] private Bonus _bonusTemplate;
+    [SerializeField] private int _bonusSpawnChance;
+    [Header("Side Wall")]
+    [SerializeField] private Transform _leftSideWall;
+    [SerializeField] private Transform _rightSideWall;
 
     private BlockSpawnPoint[] _blockSpawnPoints;
     private WallSpawnPoint[] _wallSpawnPoints;
-
+    private BonusSpawnPoint[] _bonusSpawnPoints;
 
     private void Start()
     {
         _blockSpawnPoints = GetComponentsInChildren<BlockSpawnPoint>();
         _wallSpawnPoints = GetComponentsInChildren<WallSpawnPoint>();
+        _bonusSpawnPoints = GetComponentsInChildren<BonusSpawnPoint>();
 
         for (int i = 0; i < _repeatCount; i++)
         {
@@ -36,7 +43,12 @@ public class Spawner : MonoBehaviour
             GenerateRandomElements(_wallSpawnPoints, _wallTemplate.gameObject, _wallSpawnChance, _distanceBetweenRandomLine, _distanceBetweenRandomLine / 2f);
 
             GenerateRandomElements(_blockSpawnPoints, _blockTemplate.gameObject, _blockSpawnChance, 0.15f);
+
+            GenerateRandomElements(_bonusSpawnPoints, _bonusTemplate.gameObject, _bonusSpawnChance, 0.23f);
         }
+
+        GenerateSideWall(_leftSideWall.position);
+        GenerateSideWall(_rightSideWall.position);
     }
 
     private void GenerateFullLine(SpawnPoint[] spawnPoints, GameObject generateElement)
@@ -68,5 +80,17 @@ public class Spawner : MonoBehaviour
     private void MoveSpawner(int distanceY)
     {
         transform.position = new Vector3(transform.position.x, transform.position.y + distanceY, transform.position.z);
+    }
+
+    private void GenerateSideWall(Vector3 spawnPoint)
+    {
+        float height = CalculateHeight();
+        GameObject element = Instantiate(_wallTemplate.gameObject, new Vector3(spawnPoint.x, height / 2f, spawnPoint.y), Quaternion.identity);
+        element.transform.localScale = new Vector3(element.transform.localScale.x, height, element.transform.localScale.z);
+    }
+
+    private float CalculateHeight()
+    {
+        return (_distanceBetweenRandomLine + _distanceBetweenFullLine) * _repeatCount;
     }
 }
